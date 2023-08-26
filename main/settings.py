c2f595fe58c15e914b8bd020f150f07c4f8bde4f
lucifer1708 <20bce091@nith.ac.in>
2023-08-26 17:52:05 +0530

    added changes

--- main/settings.py
+++ main/settings.py
@@ -12,14 +12,17 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
+load_dotenv()
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "&$1*c!po+ac8l489msbtw!9)flt8b)&8by+s5%q)t5*v@&0l1j"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
