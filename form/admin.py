aa504be68a55e825d67f7a7e9c6a55f43bcb1127
lucifer1708 <20bce091@nith.ac.in>
2023-08-27 13:46:04 +0530

    added changes

--- form/admin.py
+++ form/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
 
 from .models import Student
 
-# Register your models here.
 
-admin.site.register(Student)
+# Register your models here.
+class StudentAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ("name", "roll_number", "phone_number", "rollnumber_name")
+    search_fields = ("name", "roll_number", "phone_number", "rollnumber_name")
+
+
+admin.site.register(Student, StudentAdmin)
